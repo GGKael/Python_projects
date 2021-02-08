2e2480c96d0269472f03efe31cfe4e0d51cc90f5
Vivek Chowdhury <42965826+VivekChowdhury23@users.noreply.github.com>
2021-02-08 21:49:29 +0530

    Update Alexa.py

--- Alexa.py
+++ Alexa.py
@@ -1,99 +1,111 @@
+# Project1: Creating your own Alexa
+
+#importing the necessary libraries
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
-import PyDictionary
+from PyDictionary import PyDictionary
 
+#initializing the speech recognition and the Alexa voice
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-
+#First encounter
 def gesture():
-    engine.say('Hi, I am Alexa, what is your name?')
-    engine.runAndWait()
+    talk('Hi, I am Alexa, what is your name?')
     try:
         with sr.Microphone() as source:
-            print("Listening...")
+            print("Listening for your name...")
             voice = listener.listen(source)
             name = listener.recognize_google(voice)
-        engine.say('Hi ' + name + ' what can i do for you today?')
-        engine.runAndWait()
-        return name
+            talk('Hi ' + name + ', what can i do for you today?')
+        return
     except:
-        pass
+        print("Alexa could not hear you")
 
-def start(name):
-    engine.say('Hi ' + name + ' what can i do for you today?')
-    engine.runAndWait()
-
+#creating a method to let Alexa talk
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+#Speech recognition works only when you say 'Alexa'
 def command_alexa():
     try:
         with sr.Microphone() as source:
-            print("Listening...")
+            print("Listening for command...")
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
             if 'alexa' in command:
                 command = command.replace('alexa', '')
+
     except:
-        pass
+        print("Alexa could not hear you")
     return command
 
+#This flag will help me run_alexa() without the gesture()
+flag = 1
+
+# tasks that Alexa can perform
 def run_alexa():
-    gesture()
-    command = command_alexa()
-    if 'play' in command_alexa():
-        song = command.replace("play", "")
+    if flag == 1:
+        gesture()
+        task_alexa()
+    elif flag == 2:
+        task_alexa()
+
+
+def task_alexa():
+    task = command_alexa()
+    if 'play' in task:
+        song = task.replace("play", "")
         talk('Playing ' + song)
         pywhatkit.playonyt(song)
-    elif 'who is' in command or 'what is' in command:
-        person = command.replace('who is', '')
+    elif 'who is' in task or ('what is' in task and 'time' not in task):
+        person = task.replace('who is', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
-    elif 'what is the meaning of ' in command:
-        word = command.replace('what is the meaning of ', '')
+    elif 'what is the meaning of ' in task:
+        word = task.replace('what is the meaning of ', '')
         meaning = PyDictionary.meaning(word)
         print(meaning)
         talk(meaning)
-    elif 'will you go on a date' in command:
+    elif 'will you go on a date' in task:
         talk('sorry, I have a headache')
-    elif 'are you single' in command:
+    elif 'are you single' in task:
         talk('I am in a relationship with wifi')
-    elif 'joke' in command:
+    elif 'joke' in task:
         talk(pyjokes.get_joke())
-    elif 'time' in command:
+    elif 'time' in task:
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Current time is ' + time)
     else:
         talk('Hey, i did not understand')
 
+#Running Alexa, Say "Sleep Alexa" or "no Alexa" to quit.
 x = 0
 
 while x == 0:
+
     run_alexa()
-    ques = engine.say('Anything else?')
-    engine.runAndWait()
+    talk("Anything else?")
+
     try:
         with sr.Microphone() as source:
-            print("Listening...")
+            print("Listening for further instructions...")
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'sleep' in command:
-                engine.say("Good bye! Have a nice day")
-                engine.runAndWait()
+            if 'sleep' in command or 'no' in command:
+                talk("Good bye! Have a nice day")
                 x = 1
             elif 'yes' in command:
-                start()
-
+                flag = 2
     except:
-        pass
+        print("Alexa could not hear you")
